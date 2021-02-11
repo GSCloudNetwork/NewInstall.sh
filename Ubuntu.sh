f8d2ea898271769decdeb95f709ab893f868a3fc
Trevor H <73090120+synysterjake@users.noreply.github.com>
2021-02-11 00:08:37 -0600

    just testing
    datebayo

---
+++ Ubuntu.sh
@@ -0,0 +1,9 @@
+!# /bin/bash/
+
+sudo apt update & upgrade -y
+sudo apt install lynis needrestart libpam-tmpdir libpam-google-authenticator debsecan debian-goodies timeshift python3.8 python-pip lolcat rsync apt-listdiferences snap snapd locate sl
+
+sudo apt update | lolcat 
+sl
+
+echo "Fuck I farted" | lolcat 
